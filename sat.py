60b1f54a3d8cb465eb412be0f98614a011e4bd20
Stephen <stephen@umw.edu>
2023-10-20 18:27:36 -0400

    Make more OO. Debug propagate_units().

--- sat.py
+++ sat.py
@@ -8,7 +8,7 @@ class Literal():
     def __init__(self, varnum, neg):
         self.varnum = varnum
         self.neg = neg
-    def negated_form_of(selg):
+    def negated_form_of(self):
         a_copy = copy(self)
         a_copy.neg = -self.neg
         return a_copy
@@ -27,8 +27,8 @@ class Clause():
         self.lits = { Literal(abs(int(v)), np.sign(int(v)))
             for v in string.split(" ") }
     def __str__(self):
-    def remove_lit(self, lit):
         return " ∨ ".join(str(l) for l in list(self.lits))
+    def remove_literal(self, lit):
         self.lits.remove(lit)
     def is_unit(self):
         return len(self.lits) == 1
@@ -36,8 +36,11 @@ class Clause():
         return lit in self.lits
 
 class KB():
-    def __init__(self):
+    def __init__(self, filename):
         self.clauses = set()
+        with open(filename, "r", encoding="utf-8") as f:
+            for clause_line in [ l.strip() for l in f.readlines() ]:
+                self.add_clause(Clause(clause_line))
     def add_clause(self, c):
         self.clauses |= {c}
     def remove_clause(self, c):
@@ -45,29 +48,35 @@ class KB():
     def __str__(self):
         return " ∧ ".join(f"({c})" for c in list(self.clauses))
 
+    def propagate_units(self):
+        """
+        For all "unit clauses" (only one literal) perform the obvious
+        simplifications: auto-satisfy any clauses that match it, and remove
+        its negation from any clauses that match its negation.
+        """
+        for unit_clause in [ c for c in self.clauses if c.is_unit() ]:
+            the_lit = list(unit_clause.lits)[0]
+            self.remove_clause(unit_clause)
 
-def propagate_units(kb):
-    for unit_clause in [ c for c in kb.clauses if c.is_unit() ]:
-        the_lit = list(unit_clause.lits)[0]
-        kb.remove_clause(unit_clause)
+            # For every unit clause, we know that the value of its only literal
+            # is trivially set in stone. So, if there's any other clause that
+            # also has that literal, we can just get rid of it since it's
+            # already satisfied.
+            clauses_to_remove = []
+            for c in self.clauses:
+                if c.contains(the_lit):
+                    clauses_to_remove += [c]
+            for ctr in clauses_to_remove:
+                self.remove_clause(ctr)
 
-        # For every unit clause, we know that the value of its only literal is
-        # trivially set in stone. So, if there's any other clause that also has
-        # that literal, we can just get rid of it since it's already satisfied.
-        clauses_to_remove = []
-        for c in kb.clauses:
-            if c.contains(the_lit):
-                clauses_to_remove += [c]
-        for ctr in clauses_to_remove:
-            kb.remove(ctr)
-
-        # On the other hand, we also know that the negation of this literal
-        # will *never* be true. So, remove that negation in any other clause in
-        # which it occurs. If that gives an empty clause, we're doomed.
-        for c in kb.clauses:
-            negated_form = the_lit.negated_form_of()
-            if c.contains(negated_form):
-                c.remove(negated_form)
+            # On the other hand, we also know that the negation of this literal
+            # will *never* be true. So, remove that negation in any other
+            # clause in which it occurs. If that gives an empty clause, we're
+            # doomed.
+            for c in self.clauses:
+                negated_form = the_lit.negated_form_of()
+                if c.contains(negated_form):
+                    c.remove_literal(negated_form)
 
 
 if __name__ == "__main__":
@@ -78,12 +87,9 @@ if __name__ == "__main__":
     if not os.path.exists(filename):
         sys.exit(f"No such file {filename}.")
     
+    myKB = KB(sys.argv[1])
     clause = sys.argv[2]
 
-    with open(filename, "r", encoding="utf-8") as f:
-        clause_lines = [ l.strip() for l in f.readlines() ]
-
-    myKB = KB()
-
-    for clause_line in clause_lines:
-        myKB.add_clause(Clause(clause_line))
+    print(myKB)
+    myKB.propagate_units()
+    print(myKB)
