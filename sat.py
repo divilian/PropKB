3c568e7cfbdb23365fe0c07a475670dc9c4fbeed
Stephen <stephen@umw.edu>
2023-10-25 21:23:15 -0400

    Add pure_elim().

--- sat.py
+++ sat.py
@@ -35,12 +35,19 @@ class Clause():
         self.lits.remove(lit)
     def is_unit(self):
         return len(self.lits) == 1
-    def contains(self, lit):
+    def contains_literal(self, lit):
         return lit in self.lits
+    def contains_variable(self, varnum):
+        return varnum in [ l.varnum for l in self.lits ]
+    def polarity_of_variable(self, varnum):
+        for lit in self.lits:
+            if lit.varnum == varnum:
+                return lit.neg
 
 class KB():
     def __init__(self, filename):
         self.clauses = set()
+        self.assignments = {}
         with open(filename, "r", encoding="utf-8") as f:
             for clause_line in [ l.strip() for l in f.readlines() ]:
                 self.add_clause(Clause(clause_line))
@@ -58,7 +65,16 @@ class KB():
         its negation from any clauses that match its negation.
         """
         for unit_clause in [ c for c in self.clauses if c.is_unit() ]:
+            print(f"Looking at unit_clause {unit_clause}...")
             the_lit = list(unit_clause.lits)[0]
+            if the_lit.varnum in self.assignments:
+                if the_lit.neg != (self.assignments[the_lit.varnum] == 1):
+                    # Houston, we have a problem. We have at least two unit
+                    # clauses with opposite polarity!
+                    sys.exit("Houston, we have two incompatible unit clauses.")
+            self.assignments[the_lit.varnum] = (the_lit.neg == 1)
+            print(f"    propagate_units officially assigns {the_lit.varnum} "
+                f"the value {the_lit.neg == 1}")
             self.remove_clause(unit_clause)
 
             # For every unit clause, we know that the value of its only literal
@@ -67,7 +83,8 @@ class KB():
             # already satisfied.
             clauses_to_remove = []
             for c in self.clauses:
-                if c.contains(the_lit):
+                if c.contains_literal(the_lit):
+                    print(f"    Removing clause {c}")
                     clauses_to_remove += [c]
             for ctr in clauses_to_remove:
                 self.remove_clause(ctr)
@@ -78,9 +95,39 @@ class KB():
             # doomed.
             for c in self.clauses:
                 negated_form = the_lit.negated_form_of()
-                if c.contains(negated_form):
+                if c.contains_literal(negated_form):
+                    print(f"    Removing literal {negated_form} from {c}...")
                     c.remove_literal(negated_form)
 
+    #def elim_easy_doubles: TODO if the same literal appears twice in a clause
+    # with the same polarity, remove all but one for convenience. If it appears
+    # with *both* polarities, then the clause is trivially true.
+
+    def pure_elim(self):
+        """
+        For any variable that appears with only one polarity, go ahead and set
+        it to what it needs to be.
+        """
+        for varnum in Literal.varnums:
+            print(f"Looking at {varnum}...")
+            cs = [ c for c in self.clauses if c.contains_variable(varnum) ]
+            print(f"cs is {cs}")
+            pols = { c.polarity_of_variable(varnum) for c in cs }
+            if len(pols) == 0:
+                # Must have been removed by propagate_units(). Never mind.
+                pass
+            if len(pols) == 1:
+                # Great! It's pure. Eliminate it.
+                if list(pols)[0] == 1:
+                    self.assignments[varnum] = True
+                    print(f"pure_elim officially assigns {varnum} "
+                        "the value True")
+                else:
+                    self.assignments[varnum] = False
+                    print(f"pure_elim officially assigns {varnum} "
+                        "the value False")
+                for c in cs:
+                    c.remove_literal(Literal(varnum, list(pols)[0]))
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -96,3 +143,6 @@ if __name__ == "__main__":
     print(myKB)
     myKB.propagate_units()
     print(myKB)
+    myKB.pure_elim()
+    print(myKB)
+    print(f"The answer is: {myKB.assignments}")
